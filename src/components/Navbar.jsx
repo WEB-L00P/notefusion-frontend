fc1161ba165e24da99b021c7d3dd2a5c069498b2
OmkarBhosale07 <omkarbhosale5484@gmail.com>
2023-09-25 20:24:50 +0530

    Add Tailwind and Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar py-2 flex justify-between items-center">
+        <div className="hamnurgure  hover:rounded-full">
+          {/* <div className="absolute -z-[1px] w-[25px] h-[25px] hover:text-black hover:bg-white hover:rounded-full"></div> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25px"
+            height="25px"
+            fill="none"
+            strokeWidth={1.5}
+            viewBox="0 0 24 24"
+            color="#fff"
+          >
+            <path
+              stroke="#fff"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 5h18M3 12h18M3 19h18"
+            ></path>
+          </svg>
+        </div>
+        <div>
+          <img
+            src="../../public/logo.png"
+            alt="logo"
+            className="logo hover:animate-pulse"
+          />
+        </div>
+        <div className="avatar">
+          <img
+            src="../../public/user.jpg"
+            alt="avatar"
+            className="w-11 rounded-full hover:animate-spin"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
